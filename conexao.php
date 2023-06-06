0bf9f8076198002197cb9c4b91c504522eee0ddf
marcio grana <augustomarcio341@gmail.com>
2023-06-06 00:17:51 -0400

    setup database

--- conexao.php
+++ conexao.php
@@ -1,8 +1,8 @@
 <?php
-	define('HOST', 'localhost');
-	define('USER', 'root');
-	define('PASS', '');
-	define('BASE', 'produtos');
+	define('HOST', 'sql9.freemysqlhosting.net');
+	define('USER', 'sql9623965');
+	define('PASS', 'ufKMsIEhzB');
+	define('BASE', 'sql9623965');
 
 	$conn = new MySQLi(HOST,USER,PASS,BASE);
 	
